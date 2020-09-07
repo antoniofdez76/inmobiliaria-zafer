1641a39607b17f6bef14dabd2e76d71cf9530236
Jose Antonio Fernandez <jose.fernandez@udgvirtual.udg.mx>
2020-09-06 22:52:00 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+     <h1 className="title">Inmobiliaria GRUPO ZAFER <span aria-label="emoji" role="img"></span>
+     </h1>
+     <input type="text" className="new-task"/>
+  </div>
+  );
+}
+
+export default App;
